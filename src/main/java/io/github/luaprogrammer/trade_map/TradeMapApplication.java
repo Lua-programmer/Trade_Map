d8c323186a19179e2ed2799ab140c02731937cd1
Luana Melissa <luanamelissaprogrammer@gmail.com>
2023-01-17 20:55:06 -0300

    1: Subindo a aplicação para o github

---
+++ src/main/java/io/github/luaprogrammer/trade_map/TradeMapApplication.java
@@ -0,0 +1,13 @@
+package io.github.luaprogrammer.trade_map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradeMapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/luaprogrammer/trade_map/TradeMapApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.luaprogrammer.trade_map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
